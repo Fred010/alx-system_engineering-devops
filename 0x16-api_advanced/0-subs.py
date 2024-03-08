7fd73bfb30618b1b9f07ef73ed95d12db1d4da9c
Fred010 <fjbaafi18@gmail.com>
2024-03-08 08:21:34 -0800

    subs count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+a function to retrieve the number of subscribers for a given subreddit
+using the Reddit API.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Retrieve the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subs for the subreddit. Returns 0 if the
+        subreddit is invalid.
+    """
+    # Reddit API endpoint for fetching subreddit information
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+
+    # Set custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'Custom-User-Agent'}
+
+    # Send GET request to the Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the response is successful
     if response.status_code == 200:
+        # Extract number of subscribers from the response JSON
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
     else:
+        # Invalid subreddit or other error occurred
         return 0
