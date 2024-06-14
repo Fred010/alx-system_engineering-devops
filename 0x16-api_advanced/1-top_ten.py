e8e39085966f64228bdc6ee445afc9c4ac5d220a
Fred010 <fjbaafi18@gmail.com>
2024-06-14 16:06:41 -0700

    top ten redo

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print titles of first 10 hot posts listed for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (compatible; MyRedditBot/0.1)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        except (ValueError, KeyError) as e:
+            print(None)
+    else:
+        print(None)
