22e54f70722d4de7d4e3620bcc0b933227758d51
Fred010 <fjbaafi18@gmail.com>
2024-08-14 04:13:32 +0000

    0-subs again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,13 +11,18 @@ def number_of_subscribers(subreddit):
     Return the total number of subscribers on a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"user-agent": 'request'}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
     
-    data = response.json().get("data")
-    num_subs = data.get("subscribers")
+    try:
+        data = response.json().get("data", {})
+        num_subs = data.get("subscribers", 0)
+    except (ValueError, TypeError):
+        return 0
     
     return num_subs
