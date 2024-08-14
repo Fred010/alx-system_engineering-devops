e18354f4ff54ece568300bd14a79faa63300b1be
Fred010 <fjbaafi18@gmail.com>
2024-08-14 04:05:00 +0000

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,22 +7,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (compatible; MyRedditBot/0.1)"}
+    """
+    Return the total number of subscribers on a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"user-agent": 'request'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    # Debug prints to check the status code and response content
-    print(f"Status Code: {response.status_code}")
-    print(f"Response Content: {response.content}")
-
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        except (ValueError, KeyError) as e:
-            print(f"Error parsing JSON: {e}")
-            return 0
-    else:
+    if response.status_code != 200:
         return 0
+    
+    data = response.json().get("data")
+    num_subs = data.get("subscribers")
+    
+    return num_subs
